a90a412bf26847d83b4021b72b9a70abe29889a0
Angela <balderasangela@gmail.com>
2022-11-07 13:30:26 -0500

    COMPLETED ASSIGNMENT

--- index.js
+++ index.js
@@ -1,1 +1,17 @@
 // code your solution here
+function saturdayFun(activity = "roller-skate") {
+    return `This Saturday, I want to ${activity}!`
+}
+saturdayFun();
+
+function mondayWork(job = "go to the office") {
+    return `This Monday, I will ${job}.`
+}
+mondayWork();
+
+function wrapAdjective(visual="*") {
+    return function(inner="special") {
+        return `You are ${visual}${inner}${visual}!`
+    }
+}
+wrapAdjective();
